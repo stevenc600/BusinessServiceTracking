8475138c92a22af10cecc25884afbc1dfe317241
stevenc600 <stevenc600@hotmail.com>
2019-08-14 22:37:06 +1000

    Continuing to update forms and look

---
+++ BusinessServiceTracking/Controllers/AdministrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessServiceTracking.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Adminstration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
